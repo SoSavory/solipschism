a1a222be597cb742a1589968cdf9a35abfd6cfd1
Avery <amarshmellow0@gmail.com>
2017-11-24 17:04:04 -0500

    bugfixing

--- solipschism_api/app/models/coordinate.rb
+++ solipschism_api/app/models/coordinate.rb
@@ -2,6 +2,7 @@ class Coordinate < ApplicationRecord
   belongs_to :alias
 
   def self.compare_coordinates(lat1, lat2, long1, long2)
+    # Lambda is longitude, omega is latitude
     # Takes latitudes and longitudes in radians, returns distance in meters
     x = (long1 - long2)*Math.cos((lat1 + lat2)/2)
     y = lat1 - lat2
